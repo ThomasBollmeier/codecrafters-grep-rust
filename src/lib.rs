58ca9fbb76c1e403023fa8f033567e36ee10c514
Thomas Bollmeier <developer@thomas-bollmeier.de>
2025-08-15 10:51:52 +0200

    codecrafters submit [skip ci]

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,104 @@
-pub mod matcher;
-pub mod regex_parser;
+use crate::regex_parser::RegexParser;
+use clap::Parser;
+use std::{io, process};
+
+mod matcher;
+mod regex_parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+pub struct Config {
+    pub pattern: String,
+
+    #[arg()]
+    pub files_or_dirs: Vec<String>,
+
+    #[arg(short = 'E', long)]
+    pub extended_regexp: bool,
+
+    #[arg(short, long)]
+    pub recursive: bool,
+}
+
+pub fn process_stdin(pattern: &str) {
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+
+    if match_pattern(&input_line, pattern) {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+pub fn process_files_or_dirs(file_or_dirs: &[String], pattern: &str, recursive: bool) {
+    let mut found = false;
+    let filenames: Vec<String> = if recursive {
+        file_or_dirs
+            .iter()
+            .flat_map(|file_or_dir| get_files(file_or_dir))
+            .collect()
+    } else {
+        file_or_dirs.to_vec()
+    };
+
+    let multiple_files = filenames.len() > 1;
+
+    for filename in &filenames {
+        let file_content = std::fs::read_to_string(filename).unwrap();
+
+        for line in file_content.lines() {
+            if match_pattern(line, pattern) {
+                found = true;
+                if multiple_files {
+                    println!("{filename}:{line}");
+                } else {
+                    println!("{line}");
+                }
+            }
+        }
+    }
+
+    if found {
+        process::exit(0);
+    } else {
+        process::exit(1);
+    }
+}
+
+fn match_pattern(input_line: &str, pattern: &str) -> bool {
+    match RegexParser::new(pattern).parse() {
+        Ok(matcher) => matcher.matches(&input_line),
+        Err(_) => false,
+    }
+}
+
+fn get_files(file_or_dir: &str) -> Vec<String> {
+    let path = std::path::Path::new(file_or_dir);
+    if path.is_dir() {
+        match get_files_in_directory(path) {
+            Ok(files) => files
+                .into_iter()
+                .map(|p| p.to_string_lossy().to_string())
+                .collect(),
+            Err(_) => vec![],
+        }
+    } else if path.is_file() {
+        vec![file_or_dir.to_string()]
+    } else {
+        vec![]
+    }
+}
+
+fn get_files_in_directory(dir: &std::path::Path) -> io::Result<Vec<std::path::PathBuf>> {
+    let mut files = Vec::new();
+    for entry in std::fs::read_dir(dir)? {
+        let entry = entry?;
+        if entry.file_type()?.is_dir() {
+            files.extend(get_files_in_directory(&entry.path())?);
+        } else {
+            files.push(entry.path());
+        }
+    }
+    Ok(files)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,89 +1,18 @@
-use std::env;
-use std::io;
 use std::process;
-use codecrafters_grep::regex_parser::RegexParser;
+use clap::Parser;
+use codecrafters_grep::{Config, process_files_or_dirs, process_stdin};
 
-fn process_stdin(pattern: &str) {
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).unwrap();
-
-    if match_pattern(&input_line, pattern) {
-        process::exit(0);
-    } else {
-        process::exit(1);
-    }
-}
-
-fn process_file(filename: &str, pattern: &str) {
-    let file_content = std::fs::read_to_string(filename).unwrap();
-    let mut found = false;
-
-    for line in file_content.lines() {
-        if match_pattern(line, pattern) {
-            found = true;
-            println!("{line}");
-        }
-    }
-
-    if found {
-        process::exit(0);
-    } else {
-        process::exit(1);
-    }
-}
-
-fn process_files(filenames: &[String], pattern: &str) {
-    let mut found = false;
-
-    for filename in filenames {
-        let file_content = std::fs::read_to_string(filename).unwrap();
-
-        for line in file_content.lines() {
-            if match_pattern(line, pattern) {
-                found = true;
-                println!("{filename}:{line}");
-            }
-        }
-    }
-
-    if found {
-        process::exit(0);
-    } else {
-        process::exit(1);
-    }
-}
-
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
-    match RegexParser::new(pattern).parse() {
-        Ok(matcher) => matcher.matches(&input_line),
-        Err(_) => false,
-    }
-}
-
-// Usage: echo <input_text> | your_program.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    //eprintln!("Logs from your program will appear here!");
 
-    let num_args = env::args().len();
-    if num_args < 3 {
-        println!("Expected 2 or more arguments, got {}", num_args - 1);
+    let config = Config::parse();
+
+    if !config.extended_regexp {
+        println!("Option '-E' must be set to use extended regular expressions.");
         process::exit(1);
     }
 
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
-    }
-
-    let pattern = env::args().nth(2).unwrap();
-
-    match num_args {
-        3 => process_stdin(&pattern),
-        4 => process_file(&env::args().nth(3).unwrap(), &pattern),
-        _ => {
-            let filenames: Vec<String> = env::args().skip(3).collect();
-            process_files(&filenames, &pattern)
-        }
+    match config.files_or_dirs.len() {
+        0 => process_stdin(&config.pattern),
+        _ => process_files_or_dirs(&config.files_or_dirs, &config.pattern, config.recursive),
     }
 }
